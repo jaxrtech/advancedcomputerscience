5df0d52d4be56cb8cab8bdbf2794648863ee0aab
Jaxrtech <josbow@gmail.com>
2013-02-09 20:18:40 -0500

    Changed path to work with res folder

--- src/PictureTest.java
+++ src/PictureTest.java
@@ -1,6 +1,6 @@
 public class PictureTest {
     public static void main(String[] args) {
-        String file = "H:/hotel.jpg";
+        String file = "../res/hotel.jpg";
         Picture p = new Picture(file);
         p.pattern();
         p.show();
